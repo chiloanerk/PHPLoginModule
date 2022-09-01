83a38fa3d07a0c57366a29780521988a14635a9d
chiloanerk <chiloane.rk@gmail.com>
2022-09-01 18:25:49 +0200

    added files

--- login.php
+++ login.php
@@ -1,25 +1,14 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-</head>
-<body>
-    <div class="container-login">
-        <div class="container-form">
-            <form method="post">
-                <label>
-                    <input type="text" name="username" placeholder="Enter Username">
-                </label><br>
-                <label>
-                    <input type="text" name="password" placeholder="Password">
-                </label><br>
-                <button type="submit" name="submit">Submit</button>
-            </form>
-        </div>
+
+<div class="container-login">
+    <div class="container-form">
+        <form method="post">
+            <label>
+                <input type="text" name="username" placeholder="Enter Username">
+            </label><br>
+            <label>
+                <input type="text" name="password" placeholder="Password">
+            </label><br>
+            <button type="submit" name="submit">Submit</button>
+        </form>
     </div>
-</body>
-</html>
+</div>
--- signup.php
+++ signup.php
@@ -1,13 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Signup</title>
-</head>
-<body>
 <div class="container-login">
     <div class="container-form">
         <form method="post">
@@ -24,5 +14,3 @@
         </form>
     </div>
 </div>
-</body>
-</html>
