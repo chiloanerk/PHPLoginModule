11ed3c69e7fc12a646336e1798eec2c710646911
chiloanerk <chiloane.rk@gmail.com>
2022-09-01 17:40:17 +0200

    Initial commit

---
+++ signup.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Signup</title>
+</head>
+<body>
+<div class="container-login">
+    <div class="container-form">
+        <form method="post">
+            <label>
+                <input type="text" name="username" placeholder="Enter Username">
+            </label><br>
+            <label>
+                <input type="text" name="password" placeholder="Password">
+            </label><br>
+            <label>
+                <input type="text" placeholder="Verify Password">
+            </label><br>
+            <button type="submit" name="submit">Submit</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
